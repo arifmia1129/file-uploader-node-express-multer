cbeabcb56b11409985e3c2e0166ee831aa40a09b
arifvtti <arif.vtti@gmail.com>
2022-11-21 11:40:18 +0600

    send filename to db after uploaded

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const multer = require("multer");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/file-upload")
+        console.log("DB Connected")
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const userSchema = mongoose.Schema({
+    name: String,
+    image: String
+})
+
+const User = mongoose.model("User", userSchema);
+
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -26,14 +44,23 @@ app.get("/upload", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.post("/upload", upload.single("image"), (req, res) => {
+app.post("/upload", upload.single("image"), async (req, res) => {
+
+    const user = new User({
+        name: req.body.name,
+        image: req.file.filename
+    })
+
+    await user.save()
+
     res.status(200).json({
         success: true,
         message: "Successfully uploaded",
-        fileUrl: req.file.filename
+        user
     })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running at http://localhost:${PORT}`);
+    await dbConnect()
 })
